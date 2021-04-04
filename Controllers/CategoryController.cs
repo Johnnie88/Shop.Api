4a080aa9778af8e360a2e85fce4dd60940a621d5
JohnnieCode <56688518+Johnnie88@users.noreply.github.com>
2021-04-03 22:21:46 -0300

    feat(CategoryController): CRUD Build CategoryController

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -1,4 +1,7 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Shop.Models;
 
 //Endpoint = URL
 //https://localhost:5001/categories/
@@ -8,9 +11,50 @@ using Microsoft.AspNetCore.Mvc;
 [Route("categories")]
 public class CategoryController : ControllerBase
 {
+    [HttpGet]
     [Route("")]
-    public string MeuMetodo()
+    public async Task<ActionResult<List<Category>>> Get()
     {
-        return "Olá mundo";
+        return new List<Category>();
+    }
+
+    [HttpGet]
+    [Route("{id:int}")]
+    public async Task<ActionResult<Category>> GetById(int id)
+    {
+        return new Category();
+    }
+
+    [HttpPost]
+    [Route("")]
+    public async Task<ActionResult<Category>> Post([FromBody] Category model)
+    {
+        if (!ModelState.IsValid)
+        {
+            return BadRequest(ModelState);
+        }
+        return Ok(model);
+    }
+
+    [HttpPut]
+    [Route("{id:int}")]
+    public async Task<ActionResult<Category>> Put(int id, [FromBody] Category model)
+    {
+        //Verifica se o ID informado é o mesmo do modelo
+        if (model.Id != id)
+            return NotFound(new { message = "Categoria não encontrada" });
+
+        //Verifica se os dados são válidos
+        if (!ModelState.IsValid)
+            return BadRequest(ModelState);
+
+        return Ok(model);
+    }
+
+    [HttpDelete]
+    [Route("{id:int}")]
+    public async Task<ActionResult<Category>> Delete()
+    {
+        return Ok();
     }
 }
