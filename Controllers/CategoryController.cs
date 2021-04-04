15c7abf95da6e6f2451ea45315a89644f952e5f5
JohnnieCode <56688518+Johnnie88@users.noreply.github.com>
2021-04-03 23:09:50 -0300

    feat(CategoryController): SaveChangesAsync on post

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Shop.Data;
 using Shop.Models;
 
 //Endpoint = URL
@@ -27,12 +28,16 @@ public class CategoryController : ControllerBase
 
     [HttpPost]
     [Route("")]
-    public async Task<ActionResult<Category>> Post([FromBody] Category model)
+    public async Task<ActionResult<Category>> Post([FromBody] Category model,
+    [FromServices] DataContext context)
     {
         if (!ModelState.IsValid)
         {
             return BadRequest(ModelState);
         }
+        context.Categories.Add(model);
+        await context.SaveChangesAsync();
+
         return Ok(model);
     }
 
